f944f41228351df86fc01a3d33bae9618bdc86b9
trinq <triquang13@gmail.com>
2023-04-30 22:32:35 +0700

    update

--- app/api/telegram_bot.py
+++ app/api/telegram_bot.py
@@ -1,35 +1,64 @@
-import os
 import logging
-import telegram
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from dotenv import load_dotenv
-import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from app.api.retrieval import retrieve_documents
+from app.api.indexing import load_vector_store
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
 
-load_dotenv()
+# Set up logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-K = int(os.getenv("K", 5))
-BASE_URL = os.getenv("BASE_URL")
+# Load environment variables
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+# Load vector store and LLM
+vector_store = load_vector_store()
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, max_tokens=256)
+chain_type_kwargs = {"prompt": ""}
+chain = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vector_store.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi there!")
+# Define command handlers
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! I am a Q&A bot. Ask me a question using the /ask command.')
 
-def handle_message(update, context):
-    message_text = update.message.text
-    response = requests.get(f"{BASE_URL}/query_vector_store", params={"question": message_text}).json()
-    response_text = "Top results:\n\n"
-    for result in response:
-        response_text += f"{result['text']} ({result['score']:.4f})\n"
-    context.bot.send_message(chat_id=update.effective_chat.id, text=response_text)
+def ask(update: Update, context: CallbackContext) -> None:
+    """Retrieve an answer to the user's question."""
+    query = " ".join(context.args)
+    if query:
+        results = retrieve_documents(query, vector_store, chain)
+        answer = results["answer"]
+        sources = results["sources"]
+        source_urls = list(set([doc.metadata['source'] for doc in results['source_documents']]))
+        response = f"Q: {query}\n\nA: {answer}\n\nSources: {sources}\n\nAll relevant sources: {source_urls}"
+    else:
+        response = "Please provide a question to search for."
+    update.message.reply_text(response)
 
-start_handler = CommandHandler("start", start)
-message_handler = MessageHandler(Filters.text, handle_message)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(message_handler)
+def fallback(update: Update, context: CallbackContext) -> None:
+    """Fallback message for unsupported commands."""
+    update.message.reply_text('Sorry, I did not understand that command. Please use the /ask command to ask me a question.')
 
-updater.start_polling()
-updater.idle()
+# Set up handlers and start the bot
+def main() -> None:
+    updater = Updater(TELEGRAM_TOKEN)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("ask", ask))
+    dispatcher.add_handler(MessageHandler(Filters.command, fallback))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
